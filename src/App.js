e0ed213ac921c5bde78f9c8913c06d4c5b04879c
Pol <polo9421@gmail.com>
2023-07-10 22:38:05 +0200

    code source

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './components/Banner';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home";
+import Factures from "./components/Factures";
+import TVA from "./components/AnalyseTVA";
+import Perms from "./components/Perms";
+import Archives from "./components/Archives";
+import Logout from "./components/Logout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Router>
+          <Banner />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/factures' element={<Factures />} />
+            <Route path='/perms' element={<Perms/>} />
+            <Route path='/TVA' element={<TVA />} />
+            <Route path='/archives' element={<Archives />} />
+            <Route path='/logout' element={<Logout />} />
+          </Routes>
+        </Router>
+      </div>
   );
 }
 
